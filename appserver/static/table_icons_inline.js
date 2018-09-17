211fe1f30f8ba67df4a55e35865f16a3b6012a26
root <root@OD-FM-NA-i-04977a99d55a4405e.amazonaws.com>
2018-09-17 23:11:05 +0000

    re-work the questions dashboard

--- appserver/static/table_icons_inline.js
+++ appserver/static/table_icons_inline.js
@@ -13,16 +13,21 @@ require([
             var status = cell.value;
             // Compute the icon base on the field value
             var icon;
+            var color;
             if(status === "Correct" ) {
-                icon = 'check';
+                icon = 'box-checked';
+                color = '#5CC05C';
             } else if(status === "Incorrect" ) {
-                icon = 'alert-circle';
+                icon = 'error';
+                color = '#DC4E41';
             } else {
-                icon = 'alert';
+                icon = 'box-unchecked';
+                color = '#00A4FD';
             }
             // Create the icon element and add it to the table cell
-            $td.addClass('icon-inline numeric').html(_.template('<%- text %> <i class="icon-<%-icon%>"></i>', {
+            $td.addClass('icon-inline').html(_.template('<i style="color: <%-color%>;" class="icon-<%-icon%>"></i>&nbsp;<%- text %>', {
                 icon: icon,
+                color: color,
                 text: cell.value
             }));
         }
