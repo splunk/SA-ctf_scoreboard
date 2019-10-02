672b47c8db27839d3bf4f435e2440022a6d646ae
root <root@OD-FM-NA-i-050fab3ed1799eca6.amazonaws.com>
2019-10-02 22:25:46 +0000

    dark mode\!

--- appserver/static/instantiate_countdown.js
+++ appserver/static/instantiate_countdown.js
@@ -13,6 +13,9 @@ require(["jquery",
   $(panelCells[2]).css('width', '15%');
 
   // Detemine competition end time and start the clock.
+  $('#clock').css("background", "#31373e");
+  $('#clock').css("color", "#ffffff");
+
   var sm = new SearchManager({
     "id": 'getEndTimeSM',
     "cancelOnUnload": true,
@@ -26,24 +29,25 @@ require(["jquery",
     "autostart": true
     }, { tokens: true, tokenNamespace: "submitted" });
 
-    sm.on('search:done', function(properties) {
-        var searchName = properties.content.request.label
-        if (properties.content.resultCount == 0) {
-            console.log(searchName, "gave no results, so timer cannot be started.", properties)
-        } else {
-            var results = splunkjs.mvc.Components.getInstance(searchName).data('results', { output_mode: 'json', count: 0 });
-            results.on("data", function(properties) {
-                var searchName = properties.attributes.manager.id
-                var data = properties.data().results
-                epoch = parseInt(data[0]['EndTime'],10);
-                var d = new Date(0);
-                d.setUTCSeconds(epoch);
-                // Instantiate the countdown timer with the time of the last question in ctf_questions.
-                $('#clock').countdown(d, function(event) {
-                    $(this).html(event.strftime('%D:%H:%M:%S'));
-                  });
-            });
-        };
-    });
+  sm.on('search:done', function(properties) {
+      var searchName = properties.content.request.label
+      if (properties.content.resultCount == 0) {
+          console.log(searchName, "gave no results, so timer cannot be started.", properties)
+      } else {
+          var results = splunkjs.mvc.Components.getInstance(searchName).data('results', { output_mode: 'json', count: 0 });
+          results.on("data", function(properties) {
+              var searchName = properties.attributes.manager.id
+              var data = properties.data().results
+              epoch = parseInt(data[0]['EndTime'],10);
+              var d = new Date(0);
+              d.setUTCSeconds(epoch);
+              // Instantiate the countdown timer with the time of the last question in ctf_questions.
+              $('#clock').countdown(d, function(event) {
+                  $(this).html(event.strftime('%D:%H:%M:%S'));
+                });
+          });
+      };
+  });
 });
+
 
